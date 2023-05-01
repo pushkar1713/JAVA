906c9c12475bac48f645d3721ac76b096e58ec2e
pushkar1713 <119427920+pushkar1713@users.noreply.github.com>
2023-05-01 14:36:01 +0530

    1 may CW

---
+++ FACT.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class FACT {
+    public static void main(String[] args) {
+        int fact = 1; int n=0,i;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Input the number");
+        n = sc.nextInt();    
+        for(i=1;i<=n;i++){
+            fact = fact*i;
+        }
+        System.out.println("The factorial is : "+fact);
+    }
+    
+}
---
+++ table_of_2.java
@@ -0,0 +1,8 @@
+public class table_of_2 {
+    public static void main(String[] args) {
+        int i;
+        for(i=1; i<11;i++){
+        System.out.println("2x"+i+" = "+ 2*i);
+    }
+} 
+}
