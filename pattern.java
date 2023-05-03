d0689e1ede869a97e38c7c8a8641e6c668ec88d6
pushkar1713 <119427920+pushkar1713@users.noreply.github.com>
2023-05-03 15:37:29 +0530

    3 may

--- pattern.java
+++ pattern.java
@@ -2,13 +2,16 @@ import java.util.*;
 public class pattern {
     public static void main(String[] args) {
         int i=0,j=0,n=0;
-        char ch = 'a';
+        
         Scanner sc = new Scanner(System.in);
         System.out.print("Input a number : ");
         n = sc.nextInt();
         for(i=0;i<n;i++){
-            for(j=0;j<i;j++){
-                System.out.printf(" %c ",ch +j);
+            char ch = 'a';
+            for(j=0;j<i;j++)
+            {
+                System.out.print(ch);
+                ch++;
             }
             System.out.print('\n');
         }
